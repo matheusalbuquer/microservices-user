e5ee42dbebbeef648e91903d07d90335cb6972ec
matheus albuquer <mj898926@gmail.com>
2025-03-15 00:10:41 -0300

    updates

--- src/main/java/com/ms/user/models/UserModel.java
+++ src/main/java/com/ms/user/models/UserModel.java
@@ -1,11 +1,9 @@
 package com.ms.user.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @Entity
 @Table(name = "TB_USERS")
@@ -13,7 +11,7 @@ public class UserModel implements Serializable {
     private static final long serialVersionUID= 1L;
 
     @Id
-    @GeneratedValue(strategy = generationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID userId;
     private String name;
     private String email;
