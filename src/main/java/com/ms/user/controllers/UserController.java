6bfeb354dfee4c8313caaa270ada4f5d60366deb
matheus albuquer <mj898926@gmail.com>
2025-03-14 23:47:23 -0300

    updates controllers

--- src/main/java/com/ms/user/controllers/UserController.java
+++ src/main/java/com/ms/user/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.ms.user.controllers;
 
 import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
+import com.ms.user.services.UserService;
 import jakarta.validation.Valid;
 import org.apache.catalina.User;
 import org.springframework.beans.BeanUtils;
@@ -15,11 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/users")
     public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
         var userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDto,userModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save());
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userService.save(userModel)));
     }
 
 
