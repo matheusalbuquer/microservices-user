571d7ee60440a0e41bd5e403601573a4b1e32ab6
matheus albuquer <mj898926@gmail.com>
2025-03-18 21:03:01 -0300

    create methods UserProducer and create EmailDto

--- src/main/java/com/ms/user/producers/UserProducer.java
+++ src/main/java/com/ms/user/producers/UserProducer.java
@@ -1,9 +1,33 @@
 package com.ms.user.producers;
 
+import com.ms.user.models.UserModel;
+import jakarta.validation.constraints.Email;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import com.ms.user.dtos.EmailDto;
 
 @Component
 public class UserProducer {
 
-    
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer( RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value( value = "${broker.queue.email}")
+    private String routingKey;
+
+    public void publicMessageEmail(UserModel userModel){
+        var emailDto = new EmailDto();
+        emailDto.setEmailTo(userModel.getEmail());
+        emailDto.setSubject("Cadastro Realizdo Com Sucesso!");
+        emailDto.setText(userModel.getName() + "Seja Bem vindo(a)! Agradecemos o seu cadastro");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
+
+
+
 }
